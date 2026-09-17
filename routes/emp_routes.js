be7b88e6ef817752e59748230603b670a5230e0d
Praveen Reddy <saikumarreddy1258@gmail.com>
2026-09-17 14:21:15 +0530

    Second commit

---
+++ routes/emp_routes.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let router = express.Router();
+let {users}=require('../models/users');
+router.post("/register",async(req,res)=>{
+    console.log(req.body);
+    let newuser = users(req.body);
+    let result =await newuser.save();
+    res.send(result);
+});
+router.post("/login",(req,res)=>{
+    res.send("Login route");
+});
+router.get("/viewtask",(req,res)=>{
+    res.send("View tasks route");
+});
+router.put("/updatestatus",(req,res)=>{
+    res.send("Update status route");
+});
+// localhost:3000/api/emp/viewtasks GET
+module.exports=router;
---
+++ routes/hr_route.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let router = express.Router();
+router.get("/viewemployees",(req,res)=>{
+    res.send("View employees route");
+});
+router.post("/assign-task",(req,res)=>{
+    res.send("Assign tasks route");
+});
+router.get("/viewtasks",(req,res)=>{
+    res.send("View tasks route");
+});
+router.delete("/deleteEmp",(req,res)=>{
+    res.send("Delete employees route");
+});
+// localhost:3000/api/employee/viewtasks GET
+module.exports=router;
